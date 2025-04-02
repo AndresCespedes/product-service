daf771e63de9f54ed75bb1bc0cd98b34921c56fe
AndresCespedes <felipecespedes1999@hotmail.com>
2025-04-02 14:10:42 -0500

    Se crean dtos para mejorar el flujo de datos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,15 @@
-export class CreateProductDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({ example: 'Laptop', description: 'Nombre del producto' })
+    name: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(0)
+    @ApiProperty({ example: 1200.99, description: 'Precio del producto' })
+    price: number;
+}
