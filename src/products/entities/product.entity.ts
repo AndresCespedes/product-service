49b2fb4a640298b03ad0c9ed295dd381f2bf8939
AndresCespedes <felipecespedes1999@hotmail.com>
2025-04-02 14:11:30 -0500

    Se crea la entidad de productos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,13 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number;
+}
