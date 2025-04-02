d130f986dc198013b556676129d95174a8077fa3
AndresCespedes <felipecespedes1999@hotmail.com>
2025-04-02 14:07:46 -0500

    Se agrega la configuracion para swagger y para el uso del apiKey

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DataSource } from 'typeorm';
+import { ApiKeyMiddleware } from './common/auth/api-key.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(new ApiKeyMiddleware().use);
+
+  const dataSource = app.get(DataSource);
+  if (!dataSource.isInitialized) {
+    await dataSource.initialize();
+  }
+
+  const config = new DocumentBuilder()
+    .setTitle('Product API')
+    .setDescription('API para la gestión de productos')
+    .setVersion('1.0')
+    .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'x-api-key')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
