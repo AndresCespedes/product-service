658925f75c93b5b9368af82d4d74ac1cf62e1a97
AndresCespedes <felipecespedes1999@hotmail.com>
2025-04-03 16:41:11 -0500

    Se modifica para mejorar el funcionamiento del archivo y del servicio

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { dataSourceOptions } from './config/ormconfig';
-import { ApiKeyMiddleware } from './common/auth/api-key.middleware';
+import { LoggerMiddleware } from './common/logger.middleware';
 
 @Module({
   imports: [
@@ -19,8 +19,7 @@ import { ApiKeyMiddleware } from './common/auth/api-key.middleware';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(ApiKeyMiddleware)
-      .exclude('/api/docs', '/api/docs/(.*)', '/products') // Excluir Swagger
-      .forRoutes('/internal/products'); // Aplicar a todas las rutas restantes
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
   }
 }
